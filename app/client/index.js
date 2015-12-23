80cfe0c0ea8014d6bee451261c3eb2b22d86ec53
Spenser Jones <hello@spenserj.com>
2015-12-22 21:11:34 -0700

    Basic linting support and the first React component

--- app/client/index.js
+++ app/client/index.js
@@ -1,1 +1,10 @@
-console.log('test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello Samantha!</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
